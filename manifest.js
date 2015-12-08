a2f67b7b7f24af765e9dab2e6bfddc4a0391f78b
loganflashtalking <logan.cool@flashtalking.com>
2015-12-07 16:55:51 -0800

    removed useless files and added click functionality

--- manifest.js
+++ manifest.js
@@ -4,13 +4,14 @@ FT.manifest({
 	"height":720,
 	"clickTagCount":1,
 	"hideBrowsers": ["ie8"],
-	"videos": [{"name": "video1", "ref": "https://youtu.be/fGPPfZIvtCw?t=7s"}],
+	"videos": [{"name": "video1", "ref": "52272/102769_kfp2"}],
 	"instantAds":[
 		{"name":"# Card's Across (1-19)", "type":"text", "default":"5"},
 		{"name":"# Card's Down (1-19)", "type":"text", "default":"4"},
 		{"name":"Speed in Seconds (1-19)", "type":"text", "default":"1"},
 		{"name":"Web Capture Image", "type":"image", "default":"images/WC_1.png"},
 		{"name":"Ad Unit Poster", "type":"image", "default":"images/AU_1.png"},
-		{"name":"video1", "type":"text", "default":"https://youtu.be/fGPPfZIvtCw?t=7s"}
+		{"name":"video1", "type": "video"}
+		//{"name":"video1", "type":"text", "default":"https://youtu.be/fGPPfZIvtCw?t=7s"}
 	]
 });
