ad38d93b2383f95c20be8d31cabc56b8b03c5f86
loganflashtalking <logan.cool@flashtalking.com>
2015-12-07 16:16:47 -0800

    refactored interface and pixel padding

---
+++ manifest.js
@@ -0,0 +1,16 @@
+FT.manifest({
+	"filename":"index.html",
+	"width":1280,
+	"height":720,
+	"clickTagCount":1,
+	"hideBrowsers": ["ie8"],
+	"videos": [{"name": "video1", "ref": "https://youtu.be/fGPPfZIvtCw?t=7s"}],
+	"instantAds":[
+		{"name":"# Card's Across (1-19)", "type":"text", "default":"5"},
+		{"name":"# Card's Down (1-19)", "type":"text", "default":"4"},
+		{"name":"Speed in Seconds (1-19)", "type":"text", "default":"1"},
+		{"name":"Web Capture Image", "type":"image", "default":"images/WC_1.png"},
+		{"name":"Ad Unit Poster", "type":"image", "default":"images/AU_1.png"},
+		{"name":"video1", "type":"text", "default":"https://youtu.be/fGPPfZIvtCw?t=7s"}
+	]
+});
